a9f1c47e7cf47240c5cb7bb5dc86ffedae588850
SEMBADAAYUMI <sembadaayumiasyrafaufa@gmail.com>
2022-06-25 22:29:22 +0700

    membuat to do list dengan html css dan javascript

---
+++ script.js
@@ -0,0 +1,23 @@
+let input = document.querySelector("#add");
+let btn = document.querySelector("#btn");
+let list = document.querySelector("#list");
+let el = document.getElementsByTagName("li");
+//Now we will create a function that will allow us to add element on button click
+btn.addEventListener("click", () => {
+  let txt = input.value;
+  if (txt === "") {
+    alert("you must write something");
+  } else {
+    let li = document.createElement("li");
+    li.innerHTML = txt;
+    list.insertBefore(li, list.childNodes[0]);
+    input.value = "";
+  }
+});
+//now lets write the function that will allow us to mark the done subject
+//function ini tidak bekerja sebagaimana mestinya di video tutorial padahal sudah diikuti sama persis
+list.addEventListener("click", (i) => {
+  if (i.target.tagName == "LI") {
+    i.target.classList.toggle("checked");
+  }
+});
